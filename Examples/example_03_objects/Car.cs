e2deb9fcac44bf7046721b9e89a2fdfea24a18a8
Ryan Larson <larsonr7@students.westerntc.edu>
2023-03-07 12:30:01 -0600

    class

---
+++ Examples/example_03_objects/Car.cs
@@ -0,0 +1,7 @@
+public class Car {
+    public int Wheels {get;set;}
+
+    public Car(int wheels){
+        this.Wheels = wheels;
+    }
+}
---
+++ Examples/example_03_objects/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Car bug = new Car(2);
+bug.Wheels = 4;
+
+Console.WriteLine(bug.Wheels);
