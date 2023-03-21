86902190cb051f052e54057ee8e881d217b8af55
Ryan Larson <larsonr7@students.westerntc.edu>
2023-03-21 12:31:30 -0500

    class 3/21

---
+++ Examples/example_04_company/EmployeeWriter.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+
+public class EmployeeWriter{
+    public string DirPath {get; set;}
+
+    public EmployeeWriter(string DirPath){
+        this.DirPath = DirPath;
+    }
+
+
+
+}
--- Examples/example_04_company/Program.cs
+++ Examples/example_04_company/Program.cs
@@ -4,14 +4,14 @@ using System.Text.Json;
 
 string jsonFile = @"./company_info.json";
 
-var json = File.ReadAllText(jsonFile);
+ var json = File.ReadAllText(jsonFile);
 
 //Console.WriteLine(json);
 
 var options = new JsonSerializerOptions(){PropertyNameCaseInsensitive = true};
 Company microsoft = JsonSerializer.Deserialize<Company>(json, options);
 
-Employee emp = microsoft.Employees[1];
+// Employee emp = microsoft.Employees[1];
 
 //Console.WriteLine("Microsoft has " + microsoft.Employees.Length + " Employees.");
 //Console.WriteLine("Employee: " + microsoft.Employees[1].FullName);
@@ -21,18 +21,82 @@ Employee emp = microsoft.Employees[1];
 
 //Console.WriteLine($"Employee: {emp.FullName} is paid ${emp.AnnualSalary}.");
 
-var softwareEngineerLinq = new List<Employee>();
-softwareEngineerLinq = microsoft.Employees.Where(emp=> emp.Position.Description == "Software Engineer").ToList();
+// var softwareEngineerLinq = new List<Employee>();
+// softwareEngineerLinq = microsoft.Employees.Where(emp=> emp.Position.Description == "Software Engineer").ToList();
 
-foreach(var eng in softwareEngineerLinq){
-    Console.WriteLine(eng.FullName);
+// foreach(var eng in softwareEngineerLinq){
+//     Console.WriteLine(eng.FullName);
+// }
+
+
+// var moreOptions = new JsonSerializerOptions{WriteIndented = true};
+// string jsonString = JsonSerializer.Serialize(microsoft, moreOptions);
+
+// File.WriteAllText(jsonFile, jsonString);
+
+// Console.WriteLine(); 
+
+
+/**************************
+Directory Example Begin
+***************************/
+
+
+// string dirPathB = "./photo";
+// string fileName = "tester.txt";
+
+//Console.WriteLine($"Before we are currently at: {Directory.GetCurrentDirectory}");
+
+// if(Directory.Exists(dirPathB)){
+//     Console.WriteLine("Exists!");
+// }else{
+//     Console.WriteLine("Doesn't Exist!");
+//     Directory.CreateDirectory(dirPathB);
+//     Console.WriteLine("... But Now It Does!");
+// }
+
+//Directory.SetCurrentDirectory(dirPathB);
+
+// string content = $@"The names: 
+//                     - {dirPathB}
+//                     - {fileName}
+//                     are some ridiculous names";
+
+//File.WriteAllText(fileName, content);
+
+string business = "./my_employees";
+
+if(!Directory.Exists(business)){
+    Directory.CreateDirectory(business);
+}
+Directory.SetCurrentDirectory(business);
+
+Employee emp = microsoft.Employees[0];
+
+
+string empFile = $"{emp.Id}.txt";
+
+string empContent = $@"Employee Id: {emp.Id}
+Employee Name: {emp.FullName}
+Employee Annual Salary: {emp.AnnualSalary}
+Employee Position: {emp.Position.Id}
+Employee Position Description: {emp.Position.Description}
+Benefits: 
+";
+foreach(Benefit benefit in emp.Benefits){
+empContent += $@"   ID: {benefit.Id}
+    Description: {benefit.Description}
+    Additional Amount: {benefit.Additional}
+    ---------------------------
+";
 }
 
 
-var moreOptions = new JsonSerializerOptions{WriteIndented = true};
-string jsonString = JsonSerializer.Serialize(microsoft, moreOptions);
+ File.WriteAllText(empFile, empContent);
 
-File.WriteAllText(jsonFile, jsonString);
 
-Console.WriteLine();
+
+/**************************
+Directory Example End
+***************************/
 Console.WriteLine("didn't crash");
